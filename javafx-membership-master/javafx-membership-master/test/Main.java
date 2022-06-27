86b51e6783acb456bd8b11100584e0bcfe69302b
DESKTOP-9IHQ48P\505 <tnals4211@naver.com>
2022-06-27 17:22:38 +0900

    no message

--- javafx-membership-master/javafx-membership-master/test/Main.java
+++ javafx-membership-master/javafx-membership-master/test/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("member.fxml"));
 		Scene scene = new Scene(root);
 		
-		primaryStage.setTitle("학생관리프로그램");
+		primaryStage.setTitle("�븰�깮愿�由ы봽濡쒓렇�옩");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
